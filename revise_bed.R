c95da22de249451a8bf5246f3a42205f06cd64c4
Ni Shuai <nishuai@yahoo.com>
2019-07-30 14:03:45 +0800

    Smart sorting of records in a bed file
    Sorting bed file wihile avoiding chatacter comparisions, merging overlaping records, removing records with a user defined minimium length n.

--- revise_bed.R
+++ revise_bed.R
@@ -1,6 +1,5 @@
 revise_bed=function(bed, min_span=30, merge=TRUE){
   
-  bed=read.table('inputdata/cnv.anno.bed')
   bed=bed[bed[,3]-bed[,2]>=min_span,]
   nu_chr=substr(bed[,1], 4, 7)
   nu_chr=gsub( 'X', 23, nu_chr)
@@ -17,19 +16,18 @@ revise_bed=function(bed, min_span=30, merge=TRUE){
   tt=cbind(nu_chr+bed[,2], nu_chr+bed[,3])
   
   unique_range=tt[1,]
+  original_range=bed[1,]
   for (i in 1:(nrow(tt)-1)){
     if (unique_range[2]>=tt[i+1, 1]){
       ###in case the previous record totally overlaps the next record
       unique_range=c(unique_range[1], max(unique_range[2], tt[i+1, 2]))
+      original_range[3]= max(original_range[3], bed[i+1, 3])
     } else {
-      bed_list[[i]]=unique_range
+      bed_list[[i]]=original_range
       unique_range=tt[i+1,]
+      original_range=bed[i+1,]
     }
   } 
-  bed_list[[i+1]]=unique_range
+  bed_list[[i+1]]=original_range
   return(do.call(rbind, bed_list))
-  
 }
-
-
-
